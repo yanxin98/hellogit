3f173747cf2f8902402bbe32845dc7144fb61415
yanxin98 <1909753848@qq.com>
2020-09-26 15:18:33 +0800

    first

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -1,12 +1,6 @@
 public class HelloGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git.");
-        System.out.println("This is first time!");
-
-        System.out.println("test error branch");
-        System.out.println("im the main branch");
-
-        System.out.println("second");
+        System.out.println("I am branch");
     }
 }
