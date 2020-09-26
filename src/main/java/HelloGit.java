877a945efeb34625167f5a2af47b062d7764969e
yanxin98 <1909753848@qq.com>
2020-09-26 15:08:10 +0800

    first

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -7,13 +7,6 @@ public class HelloGit {
         System.out.println("test error branch");
         System.out.println("im the main branch");
 
-        System.out.println("you are error");
-
-        System.out.println("111111111111");
-
-        System.out.println("2222222222222222");
-
-        System.out.println("error");
-
+        System.out.println("ok");
     }
 }
